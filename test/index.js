bba1c4c91d70597b74620249f1f24115cb3b5154
王增迪 <wangzengdi@meituan.com>
2017-08-30 22:46:28 +0800

    add recursiveParser module unit test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,38 @@
+const R = require('ramda');
 const tape = require('tape');
 const generate = require('../src/generate');
 const getStruct = generate.getStruct;
 const recursiveParser = require('../src/recursiveParser');
+const aService = require('./AService');
+const config = require('../example/thriftmock.config');
 
+const samePath = R.curry((obja, objb, path) => R.compose(
+  R.map,
+  R.path,
+  R.split('.')
+)(path)([obja, objb]));
+
+tape('recursiveParser', t => {
+  const aServiceAst = recursiveParser(config.service[0]);
+  const aSamePath = samePath(aServiceAst, aService);
+  t.deepEqual(...aSamePath('struct.AStruct'), 'define struct in current file');
+
+  t.deepEqual(
+    ...aSamePath('include.pageVo.struct.PageVo'),
+    'include struct from different directory file'
+  );
+  t.deepEqual(
+    aService.include.pageVo.path,
+    './a/pageVo.thrift',
+    'include struct from different directory'
+  );
+
+  t.deepEqual(
+    ...aSamePath('include.pageVo.include.accountVo'),
+    'recursively include struct from different directory file'
+  );
+
+  t.end();
+});
+
+tape
